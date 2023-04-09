fadf7154ebe1246c6d7542dbb497bc0ac06adaaa
MAHMOUD ABU SULTAN - 1M.T⚛️ <95148656+mahmoud-abu-sultan@users.noreply.github.com>
2023-04-09 13:11:25 +0300

    up

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 // import { Provider } from "react-redux";
 // import store from "./store";
 import reportWebVitals from "./reportWebVitals";
-
+//1MAHMOUD.T
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
